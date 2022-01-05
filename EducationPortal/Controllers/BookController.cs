bb013a68dece92c7906892c9d1fbe8e140a81e3a
tymchenko.m <maryna.tymchenko@nixsolutions.com>
2022-01-05 17:55:48 +0200

    Added book related endpoints.

---
+++ EducationPortal/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿namespace EducationPortal.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using EducationPortal.Core.Entities;
+    using EducationPortal.Core.Interfaces;
+    using EducationPortal.ViewModels;
+    using global::AutoMapper;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly IMaterialService<BookMaterial> bookService;
+
+        private readonly IMapper mapper;
+
+        public BookController(IMaterialService<BookMaterial> bookService, IMapper mapper)
+        {
+            this.bookService = bookService;
+            this.mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAsync(BookViewModel model)
+        {
+            var book = this.mapper.Map<BookMaterial>(model);
+            await this.bookService.CreateAsync(book);
+
+            return this.Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("getAll")]
+        public async Task<IEnumerable<BookViewModel>> GetAllAsync()
+        {
+            var books = await this.bookService.GetAllMaterialsAsync();
+            return this.mapper.Map<IEnumerable<BookViewModel>>(books);
+        }
+    }
+}
